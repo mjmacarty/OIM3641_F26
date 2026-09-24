4d0a3294f77c8796dcd16bd9c69498585363a3e9
mjmacarty <matt.macarty@gmail.com>
2026-09-24 09:43:11 -0400

    class files

---
+++ python_files/stock.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import yfinance as yf
+
+class Stock:
+	pass
+
+    
+
+# --- For development testing only ---
+def main():
+	pass
+    
+
+if __name__ == '__main__':
+    main()
